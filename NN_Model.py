04432bbac7e74c94b1533873570d48b8b36d0a1e
Harry Dunn <harrydunn@Harrys-MacBook-Air-6.local>
2023-02-27 15:29:19 +0000

    Null values dealt with

--- NN_Model.py
+++ NN_Model.py
@@ -4,7 +4,7 @@ from calculate_indicators import query as query
 from calculate_indicators import main as m
 from datetime import datetime
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras import layers
 
 def organise_data(symbol):
     
@@ -41,13 +41,14 @@ def organise_data(symbol):
         training_data = training_data.append(row,ignore_index=True)
 
     #removes the frist row for the last known day of trading as we don't yet know tommorows close so it can have no taget value and thus useless training
-    #training_data.drop(index = 0,axis=0,inplace=True)
+    training_data.drop(index = 0,axis=0,inplace=True)
 
     #Removes all columns that have null values or are all zero
     for col in list(training_data.columns):
-        if training_data[str(col)].isnull().all() or training_data[str(col)].all() == 0:
+        training_data[str(col)] = training_data[str(col)].fillna(0)
+        if training_data[str(col)].all() == 0 or training_data[str(col)].isnull().any():
             training_data.drop(labels=str(col),axis=1,inplace=True)
-            
+        
 
     ########################## needs updating to split the data frame into 2 parts one for testing one for training i.e 80% train 20%test
 
@@ -65,25 +66,25 @@ def create_model(training_data):
 
     model = Sequential()
     #input dim specifies the shape of the input as the data is only one dimension dim can be used in stead of shape input_shape(dimensions,) is the same
-    model.add(Dense(128, input_shape=(dimensions,), activation='relu', name = 'input')) #setsup first hidden layer aswell as defing the input layers shape
-    model.add(Dense(128, activation='relu', name = 'Layer2'))
-    model.add(Dense(64,activation = 'relu', name='Layer3')) #another hidden layer aslo using the relu function as it is faster than the sigmoid
-    model.add(Dense(1, name = 'output'))
+    model.add(layers.Dense(128, input_shape=(dimensions,), activation='relu', name = 'input')) #setsup first hidden layer aswell as defing the input layers shape
+    model.add(layers.Dense(512, activation='relu', name = 'Layer2'))
+    model.add(layers.Dense(256, activation = 'relu', name = 'Layer3'))
+    model.add(layers.Dense(64,activation = 'relu', name='Layer4')) #another hidden layer aslo using the relu function as it is faster than the sigmoid
+    model.add(layers.Dense(1, name = 'output'))
 
     #Compile defines the loss function, the optimizer and the metrics. That's all.
-    model.compile(loss='mean_absolute_error', optimizer='adam', metrics=['mse', 'mae', 'mape'] )
+    model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mse', 'mae', 'mape'] )
 
 
     print(training_data.head(5))
+    print(training_data.info())
     print(len(training_data))
-    print(target)
     
-    model.fit(training_data,target, epochs = 250, batch_size = 10)
+    model.fit(training_data,target, epochs = 150, batch_size = 10)
 
     return
 
-def main():
-    symbol = 'GOOGL'
+def main(symbol):
     training_data = organise_data(symbol)
     print(training_data['Target'].head(10))
 
@@ -92,4 +93,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    main('GOOGL')

2f9811e14a3cc6c0ef4e9c838410b7102e0b184e
Harry Dunn <harrydunn@harrys-air-6.student.cardiff.ac.uk>
2023-03-05 14:28:03 +0000

    Controller updated to use v2 of NN model, Database updated

--- Controller.py
+++ Controller.py
@@ -1,10 +1,10 @@
-import NN_Model
+import NN_Model_2
 import get_data_2
 
 symbol = input("Ticker of company to analyse: ")
 
 get_data_2.main(symbol)
-NN_Model.main(symbol)
+NN_Model_2.main(symbol)
 
 
 print("Model created")

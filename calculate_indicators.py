ff5563619a3bd4d642a650e0d96ca01d4c0db4e7
Harry Dunn <harrydunn@harrys-air-8.student.cardiff.ac.uk>
2023-03-07 13:56:54 +0000

    Added MACD and ADX to avalible indicators

--- calculate_indicators.py
+++ calculate_indicators.py
@@ -52,6 +52,21 @@ def calc_rsi(symbol,interval):
 
     return RSI
 
+def calc_adx(symbol,interval):
+    data = query('SELECT * FROM DAILY WHERE Symbol = "{}" ORDER BY DATE ASC'.format(symbol))
+
+    ADX = pd.Series()
+    ADX = ta.ADX(data['High'],data['Low'], data['Close'], timeperiod=interval)
+    ADX.apply(lambda x: x/100) #sets ADX to be between 0-1 instead of 0-100
+    return ADX
+
+def calc_macd(symbol):
+    data = query('SELECT * FROM DAILY WHERE Symbol = "{}" ORDER BY DATE ASC'.format(symbol))
+
+    MACD = pd.Series()
+    MACD = ta.MACD(data['Close'])
+
+    return MACD
 
 #query function for sql database
 #Reuturns rows as pd dataframe
@@ -76,11 +91,14 @@ def main(symbol):
     indicators["CLOSE_SMA_7"] = price_sma(symbol,7)
     indicators["VOLUME_SMA_14"] = volume_sma(symbol,14)
     indicators["ATR_14"] = calc_atr(symbol,14)
+    indicators["ATR_7"] = calc_atr(symbol,7)
     indicators['EMA_14'] = calc_ema(symbol,14)
     indicators['EMA_7'] = calc_ema(symbol,7)
     indicators['RSI_52'] = calc_rsi(symbol,52)
     indicators['RSI_14'] = calc_rsi(symbol,14)
     indicators['RSI_7'] = calc_rsi(symbol,7)
+    indicators['ADX_14'] = calc_adx(symbol,14)
+    indicators['ADX_7'] = calc_adx(symbol,7)
 
     print("---- INDICATORS ----")
     print(indicators)
